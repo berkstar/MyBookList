9ba37b18e3f852561adc175abf24691417f62cc6
Berk Yıldız <berk465@gmail.com>
2021-05-08 15:51:53 +0300

    Spring Boot Project Initilized

---
+++ backend/src/main/java/com/G11/MyBookList/MyBookListApplication.java
@@ -0,0 +1,13 @@
+package com.G11.MyBookList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookListApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/G11/MyBookList/MyBookListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G11.MyBookList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
