3f1521f43de86409966141b6a0d729ecba4bb2f5
Berk Yıldız <berk465@gmail.com>
2021-05-11 00:16:37 +0300

    Mid-Push

--- backend_v2/routes/user.js
+++ backend_v2/routes/user.js
@@ -9,35 +9,97 @@ router.use(bodyParser.json());
 
 const tokgen = new TokenGenerator(128, TokenGenerator.BASE16);
 
-router.post("/register", async (req,res) =>{
+//Swagger Definitions
+
+
+//Schema
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      User:
+ *          type: object
+ *          required: 
+ *              - username
+ *              - email
+ *              - name
+ *              - password
+ *          properties:
+ *              user_id:
+ *                  type: integer
+ *              username:
+ *                  type: string
+ *              name:
+ *                  type: string
+ *              email:
+ *                  type: string
+ *              biography:
+ *                  type: string
+ *              password:
+ *                  type: string
+ *          example:
+ *              user_id: 1
+ *              username: Mahmut_03
+ *              email: test@test.com
+ *              name: Mahmut
+ *              password: testtset
+ */
+
+//Category
+/**
+ * @swagger
+ * tags:
+ *  name: User
+ *  description: All Users
+ */
+
+//API Methods
+/**
+ * @swagger
+ * /
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.post("/register", async (req, res) => {
 
     try {
         let username = req.body.username
+        let name = req.body.name
         let email = req.body.email
         let password = req.body.password
         var date = new Date()
-        date.setHours( date.getHours() + 1);
+        date.setHours(date.getHours() + 1);
+        
+        //Succesfully Logged in.
+        let resultAddUser = await sql.addUser(username, name, email, password);
+        if (resultAddUser && resultAddUser.affectedRows) {
 
-        let resultCheckUser = await sql.checkUser(username, password);
-            
-        //If user exists
-        if (resultCheckUser.length){
-            res.sendStatus(503);
+            sql.addAuth(date, token = tokgen.generate())
+            res.status(200);
+            res.json({
+                token: token
+            });
+        } else {// User entered mail or username that has a user.
+            res.sendStatus(406);
         }
-        else{
-            let resultAddUser = await sql.addUser(username, email, password);
-            if(resultAddUser &&resultAddUser.affectedRows){
 
-                sql.addAuth(date, token = tokgen.generate())
-                res.status(200);
-                res.json({
-                    token: token
-                    });
-            }else{// User entered duplicate mail.
-                res.sendStatus(500);
-            }
-            
-        }
+
     } catch (error) {
         res.sendStatus(500);
         console.log(error)
@@ -49,24 +111,24 @@ router.post("/register", async (req,res) =>{
 
 
 
-router.post("/login", async (req,res) =>{
+router.post("/login", async (req, res) => {
 
     try {
         let username = req.body.username
         let password = req.body.password
         var date = new Date()
-        date.setHours( date.getHours() + 1);
+        date.setHours(date.getHours() + 1);
 
         let resultCheckUser = await sql.checkUser(username, password)
         //If user exists
-        if (resultCheckUser.length){
+        if (resultCheckUser.length) {
             sql.addAuth(date, token = tokgen.generate())
             res.status(200);
             res.json({
                 token: token
-                });
+            });
         }
-        else{
+        else {
             res.sendStatus(401);
         }
 
@@ -78,15 +140,15 @@ router.post("/login", async (req,res) =>{
 })
 
 
-router.get("/logintest", async (req,res) =>{
+router.get("/logintest", async (req, res) => {
 
     try {
         let username = req.body.username
         let password = req.body.password
 
         var date = new Date()
-        date.setHours( date.getHours() + 1);
-        let result = await sql.addAuth(date,token = tokgen.generate());
+        date.setHours(date.getHours() + 1);
+        let result = await sql.addAuth(date, token = tokgen.generate());
         //res.status(500);
         //res.json(result.length);
         res.json({
@@ -96,7 +158,7 @@ router.get("/logintest", async (req,res) =>{
 
         });
         console.log("AUTH Added=>" + result.affectedRows)
-        
+
     } catch (error) {
         res.sendStatus(500);
         console.log(error)
