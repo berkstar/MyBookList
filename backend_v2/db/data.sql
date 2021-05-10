9294945fa1354a70e02022b5219b66c2403565b5
LAPTOP-O6S4DUL8\user <denizhan.kemeroz@ug.bilkent.edu.tr>
2021-05-10 22:31:45 +0300

    new files

--- backend_v2/db/data.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS User (
-  user_id int NOT NULL AUTO_INCREMENT,
-  user_name varchar(32) NOT NULL,
-  email varchar(32) NULL UNIQUE,
-  name varchar(32) NOT NULL,
-  biography varchar(255) DEFAULT NULL,
-  password varchar(32) NOT NULL,
-  PRIMARY KEY (user_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
