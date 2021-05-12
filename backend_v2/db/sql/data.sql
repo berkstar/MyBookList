378f608c7705b3a95e2fc47d77ccfd13d279a49f
Berk Yıldız <berk465@gmail.com>
2021-05-12 06:12:31 +0300

    SQL Server

--- backend_v2/db/sql/data.sql
+++ backend_v2/db/sql/data.sql
@@ -1,8 +1,16 @@
+/* USERS */
+INSERT IGNORE INTO User (name, email, name, biography, password) VALUES ("James J. Heiman", )
+
 /*Thread*/
-/*TRUNCATE TABLE Thread;*/
 INSERT IGNORE INTO Thread (name, context) VALUES ("Science Fiction", "Sci-fi is among the most popular book genre there is. With movie adaptations like Star Wars and Hitchhiker’s Guide to the Galaxy, this genre has exploded and is abundant in the book world.");
 INSERT IGNORE INTO Thread (name, context) VALUES ("Adventure","Writing a novel in the adventure category will require a trip, journey, or quest of some kind as the overall plot.");
 INSERT IGNORE INTO Thread (name, context) VALUES ("Fantasy","Fantasy encompasses a huge part of the book world. It’s one of the most popular book genres out there—a personal favorite of mine to read and write.");
 INSERT IGNORE INTO Thread (name, context) VALUES ("Science","Sci-fi is among the most popular book genre there is. With movie adaptations like Star Wars and Hitchhiker’s Guide to the Galaxy, this genre has exploded and is abundant in the book world.");
 INSERT IGNORE INTO Thread (name, context) VALUES ("Education","Writing a novel in the adventure category will require a trip, journey, or quest of some kind as the overall plot.");
 INSERT IGNORE INTO Thread (name, context) VALUES ("Romance","Fantasy encompasses a huge part of the book world. It’s one of the most popular book genres out there—a personal favorite of mine to read and write.");
+/*Posts*/
+/*INSERT IGNORE INTO Post (name, context) VALUES ("","")*/
+
+
+/*Example*/
+/* INSERT IGNORE INTO Thread (tid, user_id, title, text) VALUES ("","");*/
